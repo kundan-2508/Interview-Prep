383ca883f57910800bbd9732bf283d2fc1d52c87
Kundan Kumar <kun123658@gmail.com>
2019-07-30 13:35:55 +0530

    Create intersectionOfLinkedList.cpp

---
+++ linkedList/intersectionOfLinkedList.cpp
@@ -0,0 +1,2 @@
+https://practice.geeksforgeeks.org/problems/intersection-point-in-y-shapped-linked-lists/1
+https://www.geeksforgeeks.org/write-a-function-to-get-the-intersection-point-of-two-linked-lists/
