c8c0f51d7facafaeb73457a54f17f657bce5c15e
Kundan Kumar <kun123658@gmail.com>
2019-08-04 15:57:01 +0530

    Create sqrt decomposition.cpp

---
+++ importantAlgorithm/sqrt decomposition.cpp
@@ -0,0 +1,4 @@
+https://cp-algorithms.com/data_structures/sqrt_decomposition.html
+https://www.geeksforgeeks.org/sqrt-square-root-decomposition-technique-set-1-introduction/
+https://codeforces.com/blog/entry/23005
+https://www.hackerearth.com/practice/notes/mos-algorithm-query-square-root-decomposition/
