f60cae71f308469b5a6bf0cbf53184f350e2bf94
Kundan Kumar <kun123658@gmail.com>
2019-08-04 21:09:19 +0530

    Delete sqrt decomposition.cpp

--- importantAlgorithm/sqrt decomposition.cpp
+++
@@ -1,4 +0,0 @@
-https://cp-algorithms.com/data_structures/sqrt_decomposition.html
-https://www.geeksforgeeks.org/sqrt-square-root-decomposition-technique-set-1-introduction/
-https://codeforces.com/blog/entry/23005
-https://www.hackerearth.com/practice/notes/mos-algorithm-query-square-root-decomposition/
